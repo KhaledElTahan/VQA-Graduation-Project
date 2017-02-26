456bd4b26027c132286440f05829dc72b235dd6c
hassan ahmed <hassan ahmed>
2017-02-26 20:21:28 +0200

    Build main model

--- src/model.py
+++ src/model.py
@@ -1,31 +1,24 @@
 import tensorflow as tf
 import numpy as np
+from tensorflow.contrib import layers
 
-def model(input, batch_size=32, cell_size=512, layers_num=2):
-    sess = tf.Session()
-
-    X = tf.placeholder(tf.float32, [batch_size, None, 300]) # Batch of list of 300D Vectors
-
-    cell = tf.nn.rnn_cell.LSTMCell(cell_size, state_is_tuple=True)
+def question_lstm_model(questions_ph, cell_size, layers_num):
+    cell = tf.nn.rnn_cell.LSTMCell(cell_size, state_is_tuple=False)
     mcell = tf.nn.rnn_cell.MultiRNNCell([cell] * layers_num)
     init_state = mcell.zero_state(batch_size, tf.float32) 
-    rnn_outputs, final_state = tf.nn.dynamic_rnn(mcell, X, initial_state=init_state)
+    _, final_state = tf.nn.dynamic_rnn(mcell, questions_ph, initial_state=init_state)
 
+    combined_states = tf.stack([final_state[0],final_state[1]],1)
+    combined_states = tf.reshape(combined_states , [-1,cell_size * layers_num * 2])
+
+    return layers.relu(combined_states,1024)  # The questions features
+
+def abstrsct_model(questions_ph, img_features_ph, batch_size=32, cell_size=512, layers_num=2):
+
+    question_features = question_lstm_model(questions_ph, cell_size, layers_num)
+    img_features = layers.relu(img_features_ph,1024)
+
+    fused_features_first = tf.multiply(img_features,question_features)
+    fused_features_second = layers.relu(fused_features_first,1000)
     
-    
-    # init = tf.global_variables_initializer()
-    # sess.run(init)
-    # sess.run(final_state ,feed_dict={X:input})
-
-def test():
-    x = [1] * 300
-    y = [1] * 300
-    q1 = [x, y]
-    q2 = [x, y]
-    q3 = [x, y]
-    b = [q1, q2, q3]
-    batch = np.array(b) # 3 * 2 * 300
-
-    return model(batch, 3, 250, 2)
-
-# test()
+    return layers.linear(fused_features_second,1000)  # logits
