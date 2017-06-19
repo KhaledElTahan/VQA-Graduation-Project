0426e1eb1dd6f864ff4afa60414c0aff2e4ff8c7
Khaled El-Tahan <millerzbusiness@gmail.com>
2017-06-19 20:56:56 +0200

    Add option to use feature extraction model

--- src/feature_extraction/img_features.py
+++ src/feature_extraction/img_features.py
@@ -1,26 +1,44 @@
 import img_features_mxnet
+import img_features_tf
 
-def extract(image_batch):
+def extract(image_batch, model="MXNet"):
     """ 
-        Requires batch of 3x224x224 Images
-        Requires cells of range 0~225
-        Example on how to preprocess the image using skimago.io
+        if model == "MXNet" (Default if not assigned)
+            Requires batch of 3x224x224 Images
+            Requires cells of range 0~225
+            Example on how to preprocess the image using skimago.io
 
-        def get_image(file_name):
-            img = skimage.io.imread(file_name)
-            img = transform.resize(img, (224, 224))
-            img = np.swapaxes(img, 0, 2)
-            img = np.swapaxes(img, 1, 2)
-            return np.asarray(img) * 255.0
+            def get_image(file_name):
+                img = skimage.io.imread(file_name)
+                img = transform.resize(img, (224, 224))
+                img = np.swapaxes(img, 0, 2)
+                img = np.swapaxes(img, 1, 2)
+                return np.asarray(img) * 255.0
 
-        img_features.extract([get_image('dog'), get_image('cat')])
+            img_features.extract([get_image('dog'), get_image('cat')])
 
-        NOTE: for best performance, keep the batch size ALWAYS the SAME or use initialize_graph(max_batch_size)
+            NOTE: for best performance, keep the batch size ALWAYS the SAME or use initialize_graph(max_batch_size)
+
+        if model == "Tensorflow"
+            Requires batch of 224x224x3 Images
+            Requires cells of range 0~1
+            Example on how to preprocess the image using skimago.io
+
+            def get_image(file_name):
+                img = skimage.io.imread(file_name)
+                img = transform.resize(img, (224, 224))
+                return np.asarray(img) 
+
+            img_features.extract([get_image('dog'), get_image('cat')])
     """
-    if img_features_mxnet._BATCH_SIZE != -1 and img_features_mxnet._BATCH_SIZE != len(image_batch):
-        img_features_mxnet.change_batch_size(len(image_batch))
+    if model == "MXNet":
+        if img_features_mxnet._BATCH_SIZE != -1 and img_features_mxnet._BATCH_SIZE != len(image_batch):
+            img_features_mxnet.change_batch_size(len(image_batch))
+        my_model = img_features_mxnet
+    elif model == "Tensorflow":
+        my_model = img_features_tf
 
-    return img_features_mxnet.get_features(image_batch)
+    return my_model.get_features(image_batch)
 
 def initialize_graph(max_batch_size):
     """
@@ -31,6 +49,8 @@ def initialize_graph(max_batch_size):
 
         Typically called once, Before extracting any image features
         However, Calling it is optional and NOT required
+
+        ONLY USED WITH MXNET MODEL
     """
     if not img_features_mxnet._IS_CREATED:
         img_features_mxnet._create_model(max_batch_size)
